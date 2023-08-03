30436c95dc45fe492a0d36a7ce0300879d8730f7
Rajat <Rj799parasar@gmail.com>
2023-08-03 18:49:56 +0530

    Add about module

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -15,32 +15,30 @@ export default function TextForm(props) {
         setText(textCL);
     }
     const handleCBclick=()=>{
-        try{
-            let textCB= navigator.clipboard.writeText(text);
-            console.log(textCB);
-            setText(textCB);
-        }
-        catch(err){
-            console.log(err)
-        }
+        let textCB = document.getElementById("Text-form");
+        navigator.clipboard.writeText(textCB.value);
+    }
+    const handleRESclick=()=>{
+        let result = text.replace(/\s+/g, ' ').trim();
+        setText(result);
     }
     const handleOnChange =(event)=>{
         console.log("onchange")
         setText(event.target.value)
     }
 
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   return (
     <>
     <div className="container">
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="Text-form" rows="8"></textarea>
+        <textarea className="form-control" value={text} placeholder="Enter text here" onChange={handleOnChange} id="Text-form" rows="8"></textarea>
         <button type="button" className="btn btn-primary my-2" onClick={handleUCClick}>Convert to Upper case</button>
         <button type="button" className="btn btn-info mx-2" onClick={handleLCclick}>Convert to Lower case</button>
         <button type="button" className="btn btn-danger mx-2" onClick={handleCLclick}>Clear</button>
-        <button type="button" className="btn btn-info mx-2" onClick={handleCBclick}>Copy to clipbord</button>
-        <button type="button" className="btn btn-info mx-2">Remove extra spaces</button>
+        <button type="button" className="btn btn-info mx-2" onClick={handleCBclick} id="myBox">Copy to clipbord</button>
+        <button type="button" className="btn btn-info mx-2" onClick={handleRESclick}>Remove extra spaces</button>
       </div>
     </div>
     <div>
