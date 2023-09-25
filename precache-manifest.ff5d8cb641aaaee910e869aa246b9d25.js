f3eb03b4aa513f1da4d57ffee21c82ba06a45b80
Rajat <Rj799parasar@gmail.com>
2023-09-26 00:48:25 +0530

    Updates

---
+++ precache-manifest.ff5d8cb641aaaee910e869aa246b9d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fe11e388d882d85cadc",
+    "url": "/[your-github-username].github.io/Rajat-raj/Text_utils_React/tree/Feature1/static/css/main.9c88dea3.chunk.css"
+  },
+  {
+    "revision": "7fe11e388d882d85cadc",
+    "url": "/[your-github-username].github.io/Rajat-raj/Text_utils_React/tree/Feature1/static/js/main.7fe11e38.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/[your-github-username].github.io/Rajat-raj/Text_utils_React/tree/Feature1/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "a8bd711133ec75a30bd9",
+    "url": "/[your-github-username].github.io/Rajat-raj/Text_utils_React/tree/Feature1/static/js/2.a8bd7111.chunk.js"
+  },
+  {
+    "revision": "0a6f73a2fc4618ac84e2",
+    "url": "/[your-github-username].github.io/Rajat-raj/Text_utils_React/tree/Feature1/static/js/runtime~main.0a6f73a2.js"
+  },
+  {
+    "revision": "6f117f7f82372d855f6e5cf73816f6c8",
+    "url": "/[your-github-username].github.io/Rajat-raj/Text_utils_React/tree/Feature1/index.html"
+  }
+];
